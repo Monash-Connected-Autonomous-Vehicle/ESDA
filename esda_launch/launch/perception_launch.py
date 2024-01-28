c4a0313ad19e725f96663d87d13fbf629172864b
AbBaSaMo <95030427+AbBaSaMo@users.noreply.github.com>
2024-01-28 18:50:54 +1100

    Added slam_toolbox launch to perception_launch.py

---
+++ esda_launch/launch/perception_launch.py
@@ -0,0 +1,26 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+   
+   slam_toolbox_launch = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([os.path.join(
+         get_package_share_directory('slam_toolbox'), 'launch'),
+         '/online_async_launch.py'])
+      )
+
+  # robot_localization_launch 
+
+  # lane_detection_launch
+
+  # pothole_detection_launch
+  
+   return LaunchDescription([
+      slam_toolbox_launch
+   ])
