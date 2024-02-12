6f64d25475a9bb341b6262c5922aa6bea1d6f7fd
Jiawei Liao <jiaweiliao01@gmail.com>
2024-02-12 20:17:00 +1100

    Chore: Removed motor command publishing

--- esda_control/esda_control/paralellogram_steering_controller.py
+++ esda_control/esda_control/paralellogram_steering_controller.py
@@ -2,7 +2,6 @@ import rclpy
 from rclpy.node import Node
 
 from geometry_msgs.msg import Twist
-from msg import MotorCommand
 from dynamixel_sdk_custom_interfaces.msg import SetPosition
 
 import math
@@ -12,37 +11,18 @@ class ParallelogramSteeringController(Node):
         super().__init__('paralellogram_steering_controller')
 
         # /cmd_vel subscriber
-        self.subscription = self.create_subscription(Twist, '/cmd_vel', self.listener_callback, 10)
-
-        # /target_motor_cmd publisher (not sure if MotorCommand is the right message type)
-        self.motor_publisher = self.create_publisher(MotorCommand, '/target_motor_cmd', 10)
+        self.subscription = self.create_subscription(Twist, '/cmd_vel', self.publish_servo_position, 10)
 
         # /set_position publisher for dynamixelsdk servo
         self.servo_publisher = self.create_publisher(ServoPosition, '/set_position', 10)
-    
-    '''
-    Gets message from /cmd_vel, gets required data and calls relevant publishers
-    '''
-    def listener_callback(self, msg):
-        linear = msg.linear.x
-        angular = msg.angular.z
-
-        self.publish_motor_command(linear)
-        self.publish_servo_position(linear, angular)
-    
-    '''
-    Publishes MotorCommand message to /target_motor_cmd for mcu chip
-    '''
-    def publish_motor_command(self, linear):
-        # No limit check is needed
-        motor_command = MotorCommand()
-        motor_command.linear_velocity = linear
-        self.motor_publisher.publish(motor_command)
 
     '''
     Publishes SetPosition message to /set_position for servo
     '''
-    def publish_servo_position(self, linear, angular):
+    def publish_servo_position(self, msg):
+        linear = msg.linear.x
+        angular = msg.angular.z
+
         servo_position = SetPosition()
         # only one servo is used
         servo_position.id = 1
