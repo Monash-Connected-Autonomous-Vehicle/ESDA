82d95b4ca3fb2e53b49d7c9181cff75836c68c9a
AbBaSaMo <s.a.baaset.moslih@gmail.com>
2024-02-19 14:01:12 +1100

    added dynamixel deps in package.xml

--- esda_control/launch/parallelogram_steering_controller.launch.py
+++ esda_control/launch/parallelogram_steering_controller.launch.py
@@ -2,17 +2,25 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
+
+    servo_interface = Node(
             package='dynamixel_sdk_examples',
             executable='read_write_node',
             name='dynamixel_sdk_node',
             output='screen'
-        ),
-        Node(
+        )
+        
+    steering_controller = Node(
             package='esda_control',
             executable='parallelogram_steering_controller', 
             name='parallelogram_steering_controller',
             output='screen'
         )
+        
+    # todo mcu_interface
+    
+
+    return LaunchDescription([
+        servo_interface,
+        steering_controller
     ])
