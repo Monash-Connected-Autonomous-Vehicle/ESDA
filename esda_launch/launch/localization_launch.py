32079f10be3cf7e2a9f892163c2f1045957205d9
AbBaSaMo <s.a.baaset.moslih@gmail.com>
2024-03-13 18:49:10 +1100

    refac/docs: updated launch file indent to 4, added comments listing tasks for sensors and localization launch

--- esda_launch/launch/localization_launch.py
+++ esda_launch/launch/localization_launch.py
@@ -21,29 +21,37 @@ import launch.actions
 from launch.actions import DeclareLaunchArgument
 from ament_index_python.packages import get_package_share_directory
 
-def generate_launch_description(): # TODO update params file
+def generate_launch_description():
+
     robot_localization_dir = get_package_share_directory('robot_localization')
-    parameters_file_dir = os.path.join(robot_localization_dir, 'params')
-    parameters_file_path = os.path.join(parameters_file_dir, 'dual_ekf_navsat_example.yaml')
+    # TODO test launch with new config location
+    robot_localization_dir = get_package_share_directory('esda_launch')
+    parameters_file_dir = os.path.join(robot_localization_dir, 'config')
+    parameters_file_path = os.path.join(parameters_file_dir, 'robot_loc_dual_efk_config.yaml')
     os.environ['FILE_PATH'] = str(parameters_file_dir)
+    
     return LaunchDescription([
         launch.actions.DeclareLaunchArgument(
             'output_final_position',
-            default_value='false'),
+            default_value='false'
+        ),
+            
         launch.actions.DeclareLaunchArgument(
             'output_location',
-	    default_value='~/dual_ekf_navsat_example_debug.txt'),
-	
-    launch_ros.actions.Node(
+	        default_value='~/dual_ekf_navsat_example_debug.txt'
+	    ),
+	    
+        launch_ros.actions.Node(
             package='robot_localization', 
             executable='ekf_node', 
             name='ekf_filter_node_odom',
 	        output='screen',
             parameters=[parameters_file_path],
             remappings=[('imu/data', 'imu'),
-                        ('odometry/filtered', 'odometry/local')]           
-           ),
-    launch_ros.actions.Node(
+                        ('odometry/filtered', 'odometry/local')
+        ]),
+               
+        launch_ros.actions.Node(
             package='robot_localization', 
             executable='ekf_node', 
             name='ekf_filter_node_map',
@@ -53,9 +61,10 @@ def generate_launch_description(): # TODO update params file
                         ('gps/fix', 'navsatfix'), 
                         ('gps/filtered', 'gps/filtered'),
                         ('odometry/gps', 'odometry/gps'),
-                        ('odometry/filtered', 'odometry/global')]
-           ),           
-    launch_ros.actions.Node(
+                        ('odometry/filtered', 'odometry/global')
+        ]),    
+                      
+        launch_ros.actions.Node(
             package='robot_localization', 
             executable='navsat_transform_node', 
             name='navsat_transform',
@@ -65,6 +74,7 @@ def generate_launch_description(): # TODO update params file
                         ('gps/fix', 'navsatfix'), 
                         ('gps/filtered', 'gps/filtered'),
                         ('odometry/gps', 'odometry/gps'),
-                        ('odometry/filtered', 'odometry/global')]
-           )           
-])
+                        ('odometry/filtered', 'odometry/global')
+        ])           
+    ])
+
