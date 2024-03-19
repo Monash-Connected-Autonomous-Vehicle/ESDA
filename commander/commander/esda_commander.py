07d897e87a1733d98533850dfbf1b28d33a6516f
Anthony Oon <zoon0002@student.monash.edu>
2024-03-19 14:47:48 +1100

    small update

--- commander/commander/esda_commander.py
+++ commander/commander/esda_commander.py
@@ -1,19 +1,17 @@
-import time
 import rclpy
 from rclpy.node import Node
 from typing import List
-
 from std_msgs.msg import String
 # from actions_msgs.msg import GoalStatus
-from geometry_msgs.msg import PoseStamped, Pose
+from geometry_msgs.msg import PoseStamped, Pose, PoseArray
 from rclpy.action import ActionClient
 # from rclp.duration import Duration
 from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
 from nav2_msgs.action import NavigateThroughPoses
-from nav2_msgs.action._navigate_through_poses import (
-    NavigateThroughPoses_FeedbackMessage,
-    NavigateThroughPoses_GetResult_Response,
-    NavigateThroughPoses_SendGoal_Response)
+# from nav2_msgs.action._navigate_through_poses import (
+# NavigateThroughPoses_FeedbackMessage,
+# NavigateThroughPoses_GetResult_Response,
+# NavigateThroughPoses_SendGoal_Response)
 
 
 def createPoseStamped(x, y, z, timestamp, frame="map") -> PoseStamped:
@@ -31,13 +29,19 @@ def createPoseStamped(x, y, z, timestamp, frame="map") -> PoseStamped:
 class ESDACommander(Node):
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
         self.nav_through_poses_client = ActionClient(
             self, NavigateThroughPoses, "navigate_through_poses")
         self.pose_sub = self.create_subscription(
             PoseStamped,
             "odometry/local",
             self._pose_callback, 1)
+        # Waypoints may require custom message type to define acceptance criteria
+        self.waypoints_sub = self.create_subscription(
+            PoseArray,
+            "navigation/waypoints",
+            self._waypoints_callback,
+            1
+        )
         self.navigator = BasicNavigator()
         # Await Nav2Setup before completing construction
         self.navigator.lifecycleStartup()
@@ -50,40 +54,32 @@ class ESDACommander(Node):
         # TODO: Add a waypoints subscriptions instead of hardcode
 
         # For now
-        self.pose = createPoseStamped(0, 0, 0, self.get_clock().now().to_msg())
         self.waypoints = [createPoseStamped(-2, -1, 0, self.get_clock().now().to_msg()), createPoseStamped(-2, 1, 0, self.get_clock().now().to_msg())]
 
     def _pose_callback(self, msg: PoseStamped):
         self.pose = msg
 
+    def _waypoints_callback(self, msg: PoseArray):
+        self.waypoints = msg.poses
+        self.navigator.waitUntilNav2Active()
+        if not self.navigator.isTaskComplete():
+            self.navigator.cancelTask()
+            self.get_logger().info("Cancelled task for more recent waypoints")
+        self.navigator.goThroughPoses(self.waypoints)
+        self.get_logger().info(
+            f"Beginning navigate through {len(self.waypoints)} starting from {self.pose.pose.position}")
+
     def timer_callback(self):
-        if self.pose is None:
-            self.get_logger().info("Not ready, missing initial pose")
-            return
         if len(self.waypoints):
             self.get_logger().info("Not ready, missing waypoints")
         if not self.navigator.isTaskComplete():
+            feedback = self.navigator.getFeedback()
+            print(f"In progress{feedback}")
             return
 
         self.navigator.goThroughPoses(self.waypoints)
         self.get_logger().info(
             f"Beginning navigate through {len(self.waypoints)} starting from {self.pose.pose.position}")
-
-        while not self.navigator.isTaskComplete():
-            feedback = self.navigator.getFeedback()
-            print(f"In progress")
-            result = self.navigator.getResult()
-
-            match result:
-                case TaskResult.SUCCEEDED:
-                    self.get_logger().info("Goal succeeded")
-                case TaskResult.CANCELED:
-                    self.get_logger().info("Goal was cancelled")
-                case TaskResult.FAILED:
-                    self.get_logger().warn("Goal failed")
-                case _:
-                    self.get_logger().warn(f"Unknown task result")
-            time.sleep(1)
 
 
 def main(args=None):
