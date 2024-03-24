9dbeccd4baa375455d18fbfd7bd326a5c2a0204f
Anthony Oon <zoon0002@student.monash.edu>
2024-03-24 16:03:38 +1100

    Occupancy grid fusing using numpy arrays and scipy convolve with dummy publishers for testing

--- occupancy-fuser/occupancy-fuser/dummy_grid_pub.py
+++ occupancy-fuser/occupancy-fuser/dummy_grid_pub.py
@@ -1,4 +1,6 @@
+from array import array as Array
 from nav_msgs.msg import OccupancyGrid, MapMetaData
+
 import rclpy
 from rclpy.node import Node
 import numpy as np
@@ -10,8 +12,7 @@ class OccupancyPub(Node):
         self.grid1_pub= self.create_publisher(
             OccupancyGrid,
             "grid1",
-        	 0)
-        
+        	 0) 
         ## The different occupancy grids may of different sizes but are assumed to be local centred???
         self.grid2_pub = self.create_publisher(
             OccupancyGrid,
@@ -28,13 +29,14 @@ class OccupancyPub(Node):
         self.grid1_pub.publish(grid1)
         self.grid2_pub.publish(grid2)
 		
-    def make_grid(self) -> OccupancyGrid:
+    def make_grid(self, info:MapMetaData=None) -> OccupancyGrid:
         grid = OccupancyGrid()
 
-        data = np.ones((51, 51), dtype='int')
-        grid.data = [1]*51**2
+        data = np.ones((51, 51), dtype='int8')*50
+        grid.data = Array('b', data.ravel().astype(np.int8))
         grid.header.frame_id = "base_link"
         grid.header.stamp = self.get_clock().now().to_msg()
+        grid.info = info or MapMetaData()
         grid.info.width = data.shape[0]
         grid.info.height = data.shape[1]
 
--- occupancy-fuser/occupancy-fuser/occupancy-fuser.py
+++ occupancy-fuser/occupancy-fuser/occupancy-fuser.py
@@ -1,3 +1,5 @@
+from array import array as Array
+import scipy as sp
 from nav_msgs.msg import OccupancyGrid, MapMetaData
 import rclpy
 from rclpy.node import Node
@@ -8,16 +10,19 @@ class OccupancyFuser(Node):
         super().__init__('occupancy_fuser')
         self.grid1_sub= self.create_subscription(
             OccupancyGrid,
-            "map",
+            "grid1",
             self._update_callback, 1)
         
         ## The different occupancy grids may of different sizes but are assumed to be local centred???
         self.grid2_sub = self.create_subscription(
             OccupancyGrid,
-            "camera_map",
+            "grid2",
             self._update_callback,
             1
         )
+        self.pub = self.create_publisher(OccupancyGrid,
+                                         "/grid_fused",
+                                         1)
         self.timer = self.create_timer(1, self.timer_callback)
         self.recent_grids = dict()
 
@@ -32,16 +37,31 @@ class OccupancyFuser(Node):
     def timer_callback(self):
         if len(self.recent_grids) == 0:
             self.get_logger().info("Not ready, no occupancy grids received")
+        if not self.recent_grids:
+            return
         grid = OccupancyGrid()
         grid.header.frame_id = "base_link"
-        grid.header.timestamp = self.get_clock().now().to_msg()
-        data = np.array(size=(51, 51))
-        for map in self.recent_grids:
-            if self.get_clock().now() - map.header.timestamp > 1:
+        grid.header.stamp = self.get_clock().now().to_msg()
+
+        width = max(self.recent_grids.values(), key=lambda x: x.info.width).info.width
+        height = max(self.recent_grids.values(), key=lambda x:x.info.height).info.height
+        data = np.zeros((width, height))
+        # Create a kernel which has a single island in the centre, use this to splat all submaps into main map
+        centre_kernel = np.zeros(data.shape)
+        centre_kernel[width//2+1, height//2+1] = 1
+        for map in self.recent_grids.values():
+            if self.get_clock().now().seconds_nanoseconds()[0] - map.header.stamp.sec > 1:
                 self.get_logger().info(f"Discarding old map data from frame {map.header.frame_id}")
                 continue
-             map_data = np.array(map.data)
-
+            map_data = np.array(map.data)
+            map_data.shape = (map.info.width, map.info.height)
+            data += sp.signal.convolve2d(centre_kernel, map_data, mode='same')
+        grid.data = Array('b', map_data.ravel().astype(np.int8))
+        grid.info = MapMetaData()
+        grid.info.height = height
+        grid.info.width = width
+        self.pub.publish(grid)
+        
 
 
 def main(args=None):
