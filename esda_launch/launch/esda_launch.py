903239c737f67ee19c77c4cc271dad7370b5b6f2
AbBaSaMo <s.a.baaset.moslih@gmail.com>
2024-07-02 16:30:04 +1000

    refactoring launch files

--- esda_launch/launch/esda_launch.py
+++ esda_launch/launch/esda_launch.py
@@ -9,30 +9,46 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
+   # TODO remove based on where it is launched elsewhere
+   zed_wrapper_launch = IncludeLaunchDescription(
+       PythonLaunchDescriptionSource([os.path.join(
+           get_package_share_directory('zed_wrapper'), 'launch'),
+           '/zed_camera.launch.py']
+       ),
+       launch_arguments={'camera_model': 'zed2i'}.items()
+   )
+
+   rsp_launch = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([os.path.join(
+         get_package_share_directory('esda_sim'),'launch','rsp.launch.py'
+      )]), launch_arguments={'use_sim_time': 'true'}.items()
+   )
    
    sensors_launch = IncludeLaunchDescription(
       PythonLaunchDescriptionSource([os.path.join(
          get_package_share_directory('esda_launch'), 'launch'),
          '/sensors_launch.py'])
-      )
+   )
    
    localization_launch = IncludeLaunchDescription(
       PythonLaunchDescriptionSource([os.path.join(
          get_package_share_directory('esda_launch'), 'launch'),
          '/localization_launch.py'])
-      )
+   )
    
    perception_launch = IncludeLaunchDescription(
       PythonLaunchDescriptionSource([os.path.join(
          get_package_share_directory('esda_launch'), 'launch'),
          '/perception_launch.py'])
-      )
+   )
 
    # planning launch = ...
    
    # control launch = ...
    
    return LaunchDescription([
+      zed_wrapper_launch,
+      rsp_launch,
       sensors_launch,
       localization_launch,
       perception_launch
--- esda_launch/launch/sensors_launch.py
+++ esda_launch/launch/sensors_launch.py
@@ -10,7 +10,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 def generate_launch_description():
     swiftnav_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('swiftnav-ros2'), 'launch'),
+            get_package_share_directory('swiftnav_ros2_driver'), 'launch'),
            '/start.py']
         )
     )
@@ -21,17 +21,8 @@ def generate_launch_description():
             '/velodyne-all-nodes-VLP16-launch.py']
         )
     )
-
-    zed_wrapper_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('zed_wrapper'), 'launch'),
-            '/zed_camera.launch.py']
-        ),
-        launch_arguments={'camera_model': 'zed2i'}.items()
-    )
-
+   
     return LaunchDescription([
         swiftnav_launch,
-        velodyne_nodes_launch,
-        zed_wrapper_launch
+        velodyne_nodes_launch
     ])
