f49983c0ad80083b6051adce25962ab9cbd95f13
AbBaSaMo <s.a.baaset.moslih@gmail.com>
2024-07-02 14:29:35 +1000

    re-added forked repos to esda.repos cause OG do not allow for passing custom configs

--- esda_launch/launch/sensors_launch.py
+++ esda_launch/launch/sensors_launch.py
@@ -8,42 +8,30 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
-   
     swiftnav_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('esda_launch'), 'launch'),
-           '/piksi_launch.py'])
+            get_package_share_directory('swiftnav-ros2'), 'launch'),
+           '/start.py']
         )
+    )
 
-    velodyne_driver_launch = IncludeLaunchDescription(
+    velodyne_nodes_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('esda_launch'), 'launch'),
-            '/lidar_launch.py'])
+            get_package_share_directory('velodyne'), 'launch'),
+            '/velodyne-all-nodes-VLP16-launch.py']
         )
-
-    velodyne_pcl_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('velodyne_pointcloud'), 'launch'),
-            '/velodyne_transform_node-VLP16-launch.py'])
-        )
-      
-    velodyne_scan_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('velodyne_laserscan'), 'launch'),
-            '/velodyne_laserscan_node-launch.py'])
-        )
+    )
 
     zed_wrapper_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('zed_wrapper'), 'launch'),
-            '/zed_camera.launch.py']),
-        launch_arguments={'camera_model': 'zed2i'}.items(),
+            '/zed_camera.launch.py']
+        ),
+        launch_arguments={'camera_model': 'zed2i'}.items()
     )
 
     return LaunchDescription([
         swiftnav_launch,
-        velodyne_driver_launch,
-        velodyne_pcl_launch,
-        velodyne_scan_launch,
+        velodyne_nodes_launch,
         zed_wrapper_launch
     ])
