21068d5ee9afa0c341ebb550e8827050b526f78d
Credit3 <stri0019@sudent.monash.edu>
2024-08-23 18:24:32 +1000

    Made a test mcu interface code primarily used for testing. Will still need to test for serial

--- esda_control/esda_control/mcu_interface.py
+++ esda_control/esda_control/mcu_interface.py
@@ -123,98 +123,98 @@
 
 
 
-# import rclpy
-# from rclpy.node import Node
-# from geometry_msgs.msg import Twist
-# from enum import Enum
-# import serial
-# import struct
-# import serial.tools.list_ports
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from enum import Enum
+import serial
+import struct
+import serial.tools.list_ports
 
-# ports = serial.tools.list_ports.comports()
-# for port in ports:
-#     print(port.device)
+ports = serial.tools.list_ports.comports()
+for port in ports:
+    print(port.device)
 
-# print("i")
 
-# class ESDACANMessage(Enum):
-#     SetTargetVelLeft = 1
-#     SetTargetVelRight = 2
-#     CurrentVelLeft = 3
-#     CurrentVelRight = 4
-#     CurrentDSPLeft = 5
-#     CurrentDSPRight = 6
-#     MCUState = 16
-#     MCUErrorState = 17
-#     ESTOP = 8
 
-# class MCU_Interface(Node):
-#     def __init__(self, use_serial=False):
-#         super().__init__('mcu_interface')
-#         self.use_serial = use_serial
-#         self.subscription = self.create_subscription(Twist, '/cmd_vel', self.cmd_vel_callback, 10)
-#         if self.use_serial:
-#             self.connect_to_usb_to_can_device()
+class ESDACANMessage(Enum):
+    SetTargetVelLeft = 1
+    SetTargetVelRight = 2
+    CurrentVelLeft = 3
+    CurrentVelRight = 4
+    CurrentDSPLeft = 5
+    CurrentDSPRight = 6
+    MCUState = 16
+    MCUErrorState = 17
+    ESTOP = 8
 
-#     def connect_to_usb_to_can_device(self):
-#         serial_port = '/dev/ttyUSB0'
-#         baud_rate = 115200
-#         self.serial_connection = serial.Serial(serial_port, baud_rate, timeout=1, bytesize=8)
+class MCU_Interface(Node):
+    def __init__(self, use_serial=False):
+        super().__init__('mcu_interface')
+        self.use_serial = use_serial
+        self.subscription = self.create_subscription(Twist, '/cmd_vel', self.cmd_vel_callback, 10)
+        if self.use_serial:
+            self.connect_to_usb_to_can_device()
+
+    def connect_to_usb_to_can_device(self):
+        serial_port = '/dev/ttyUSB1'
+        baud_rate = 115200
+        self.serial_connection = serial.Serial(serial_port, baud_rate, timeout=1, bytesize=8)
     
-#     def cmd_vel_callback(self, msg):
-#         linear_vel = msg.linear.x
-#         self.send_can_msg(linear_vel, ESDACANMessage.SetTargetVelLeft)
-#         self.send_can_msg(linear_vel, ESDACANMessage.SetTargetVelRight)
+    def cmd_vel_callback(self, msg):
+        linear_vel = msg.linear.x
+        self.send_can_msg(linear_vel, ESDACANMessage.SetTargetVelLeft)
+        self.send_can_msg(linear_vel, ESDACANMessage.SetTargetVelRight)
 
-#     def send_can_msg(self, msg, ID):
-#         # Log the received message instead of sending it via serial
-#         self.get_logger().info("Received Twist message: Linear.x = %f" % msg)
+    def send_can_msg(self, msg, ID):
+        # Log the received message instead of sending it via serial
+        self.get_logger().info("Received Twist message: Linear.x = %f" % msg)
         
         
-#         # Bypass serial communication for testing
-#         if self.use_serial:
-#             packet = bytearray()
-#             packet.append(ID.value)
-#             data = bytearray(struct.pack('<f', msg))
-#             packet.extend(data)
+        # Bypass serial communication for testing
+        if self.use_serial:
+            packet = bytearray()
+            packet.append(ID.value)
+            data = bytearray(struct.pack('<f', msg))
+            packet.extend(data)
 
-#             try:
-#                 self.serial_connection.write(packet)
-#             except serial.SerialException as e:
-#                 self.get_logger().error(f"Error writing to serial connection: {e}")
+            try:
+                self.serial_connection.write(packet)
+            except serial.SerialException as e:
+                self.get_logger().error(f"Error writing to serial connection: {e}")
 
-#     def spin(self):
-#         while rclpy.ok():
-#             rclpy.spin_once(self)
+    def spin(self):
+        while rclpy.ok():
+            rclpy.spin_once(self)
 
-#             if self.use_serial:
-#                 try:
-#                     log_message = "Receiving data from CAN:\n"
-#                     CAN_data = self.serial_connection.read(5)
-#                     if CAN_data:
-#                         ID = struct.unpack('B', CAN_data[0:1])[0]
-#                         data = struct.unpack('f', CAN_data[1:5])[0]
-#                         log_message += f"ID: {ID}\nValue: {data}\n"
-#                     else:
-#                         log_message += "No message received"
-#                     self.get_logger().info(log_message)
+            if self.use_serial:
+                try:
+                    log_message = "Receiving data from CAN:\n"
+                    CAN_data = self.serial_connection.read(5)
+                    if CAN_data:
+                        ID = struct.unpack('B', CAN_data[0:1])[0]
+                        data = struct.unpack('f', CAN_data[1:5])[0]
+                        log_message += f"ID: {ID}\nValue: {data}\n"
+                    else:
+                        log_message += "No message received"
+                    self.get_logger().info(log_message)
 
-#                 except Exception as e:
-#                     self.get_logger().error("Error reading from serial: %s" % str(e))
+                except Exception as e:
+                    self.get_logger().error("Error reading from serial: %s" % str(e))
 
-#     def destroy_node(self):
-#         if self.use_serial and self.serial_connection.is_open:
-#             self.serial_connection.close()
-#         super().destroy_node()
+    def destroy_node(self):
+        if self.use_serial and self.serial_connection.is_open:
+            self.serial_connection.close()
+        super().destroy_node()
 
-# def main(args=None):
-#     rclpy.init(args=args)
-#     mcu_interface = MCU_Interface(use_serial=False)  # Set use_serial=False for testing without serial
-#     mcu_interface.spin()
-#     mcu_interface.destroy_node()
-#     rclpy.shutdown()
+def main(args=None):
+    rclpy.init(args=args)
+    mcu_interface = MCU_Interface(use_serial=False)  # Set use_serial=False for testing without serial
+    mcu_interface.spin()
+    mcu_interface.destroy_node()
+    rclpy.shutdown()
     
     
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
